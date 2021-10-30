fd1f7850641bab8c8100a1a97a61ef74ed8221c6
Fardin Kibria <fardin_kibria@tjxcanada.ca>
2021-10-30 12:33:34 -0400

    async/await, promises learning

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,57 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        setText(JSON.stringify(data)); //SetText places it on the results place and then the data is turned into a string
+    });
 }
 
 export function getCatch() {
+    axios.get("http://localhost:3000/orders/123")
+    .then(({data}) => {
+        setText(JSON.stringify(data)); //SetText places it on the results place and then the data is turned into a string
+    })
+
+    .catch(err => setText(err)); // to handle rejected state
 }
 
 export function chain() {
+    axios.get("http://localhost:3000/orders/123")
+    .then(({data}) => {return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+
+    .then(({data}) => {
+        setText(`City: ${data.city}`)
+    });
 }
 
 export function chainCatch() {
+
+    axios.get("http://localhost:3000/orders/123")
+    .then(({data}) => {return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+
+    .then(({data}) => {
+        setText(`City: ${data.city}`)
+    })
+    .catch(err => setText(err));
 }
 
 export function final() {
+    axios.get("http://localhost:3000/orders/123")
+    .then(({data}) => {return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+
+    .then(({data}) => {
+        setText(`City: ${data.city}`)
+    })
+    .catch(err => setText(err))
+    .finally(() => {
+        setTimeout(() => {
+            hideWaiting();
+        }, 1500)
+
+        appendText("TJX Bootcamp Training. Done");
+    })
 }
